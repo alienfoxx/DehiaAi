31e0aa51a7f71e2a643772d819498a51eae35fd8
alienfoxx <alienfoxx001@gmail.com>
2024-05-18 23:21:51 +0100

    add the  brand section/componenet

--- src/component/brand/Brand.jsx
+++ src/component/brand/Brand.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import'./brand.css'
+import React from "react";
+import "./brand.css";
+import { google, slack, atlassian, dropbox, shopify } from "./import";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="app__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlassin" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
