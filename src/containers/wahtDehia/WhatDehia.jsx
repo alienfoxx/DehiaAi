a0c2c978d5f404a43e670f75df9d5b9f0794053d
alienfoxx <alienfoxx001@gmail.com>
2024-05-18 23:58:41 +0100

    add the whatgpt3 section/componenet

--- src/containers/wahtDehia/WhatDehia.jsx
+++ src/containers/wahtDehia/WhatDehia.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './whatDehia.css'
+import { Feauture } from '../../component'
 
 const WhatDehia = () => {
   return (
-    <div>WhatDehia</div>
+    <div className="app__whatgpt3 section__margin" id='wgpt3'>
+      <div className="app__whatgpt3-feature">
+        <Feauture/>
+      </div>
+      <div className="app__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="app__whatgpt3-container">
+        <Feauture/>
+        <Feauture/>
+        <Feauture/>
+      </div>
+    </div>
   )
 }
 
