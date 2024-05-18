3f62141e3828a7a09525a0c8c8f328f252dd3c70
alienfoxx <alienfoxx001@gmail.com>
2024-05-18 12:14:23 +0100

    fix the Navbar and make it responsive/ ad the mobile navbar

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,27 +1,29 @@
-import React from "react";
-import "./navbar.css";
+import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assests/logo.svg";
+import "./navbar.css";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="app__navbar">
       <div className="app__navbar-links">
-        {/* <div className="app__navbar-links_logo">
-          <h2>Dehia.ai</h2>
-        </div> */}
-        <div className="app__navbar-links__container">
+        <div className="app__navbar-links_logo">
+          <img src={logo} />
+        </div>
+        <div className="app__navbar-links_container">
           <p>
             <a href="#home">Home</a>
           </p>
           <p>
-            <a href="#wdehia">What is Dehia</a>
+            <a href="#wgpt3">What is GPT3?</a>
           </p>
           <p>
-            <a href="#possibility">Dehia AI</a>
+            <a href="#possibility">Open AI</a>
           </p>
           <p>
-            <a href="#features">Case studies</a>
+            <a href="#features">Case Studies</a>
           </p>
           <p>
             <a href="#blog">Library</a>
@@ -29,8 +31,48 @@ const Navbar = () => {
         </div>
       </div>
       <div className="app__navbar-sign">
-        <p>Sign In</p>
-        <button type="button">Sign Up</button>
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="app__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="app__navbar-menu_container scale-up-center">
+            <div className="app__navbar-menu_container-links">
+              <p>
+                <a href="#home">Home</a>
+              </p>
+              <p>
+                <a href="#wgpt3">What is GPT3?</a>
+              </p>
+              <p>
+                <a href="#possibility">Open AI</a>
+              </p>
+              <p>
+                <a href="#features">Case Studies</a>
+              </p>
+              <p>
+                <a href="#blog">Library</a>
+              </p>
+            </div>
+            <div className="app__navbar-menu_container-links-sign">
+              <p>Sign in</p>
+              <button type="button">Sign up</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
