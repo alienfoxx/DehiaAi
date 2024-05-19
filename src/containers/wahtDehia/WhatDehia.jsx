237afc31392da112e81aedbf587902be1e8906fc
alienfoxx <alienfoxx001@gmail.com>
2024-05-19 11:59:25 +0100

    add the whatgpt section/componenet full responsive erorre fixed

--- src/containers/wahtDehia/WhatDehia.jsx
+++ src/containers/wahtDehia/WhatDehia.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import './whatDehia.css'
-import { Feauture } from '../../component'
+import React from "react";
+import "./whatDehia.css";
+import { Feauture } from "../../component";
 
 const WhatDehia = () => {
   return (
-    <div className="app__whatgpt3 section__margin" id='wgpt3'>
-      <div className="app__whatgpt3-feature">
-        <Feauture/>
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feauture
+          title="What is GPT-3"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
       </div>
-      <div className="app__whatgpt3-heading">
-        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
-        <p>Explore The Library</p>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore the Library</p>
       </div>
-      <div className="app__whatgpt3-container">
-        <Feauture/>
-        <Feauture/>
-        <Feauture/>
+      <div className="gpt3__whatgpt3-container">
+        <Feauture
+          title="Chatbots"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought."
+        />
+        <Feauture
+          title="Knowledgebase"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
+        <Feauture
+          title="Education"
+          text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WhatDehia
+export default WhatDehia;
