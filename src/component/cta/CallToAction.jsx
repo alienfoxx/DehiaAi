88653af694e26bb143bd04240b548e6a1a28b519
alienfoxx <alienfoxx001@gmail.com>
2024-05-19 14:47:58 +0100

    adding the cta section/compoenenet fully responsive all is done

--- src/component/cta/CallToAction.jsx
+++ src/component/cta/CallToAction.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import'./cta.css'
+import React from "react";
+import "./cta.css";
 
 const CallToAction = () => {
   return (
-    <div>Article</div>
-  )
-}
+    <div className="app__cta">
+      <div className="app__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="app__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default CallToAction
+export default CallToAction;
